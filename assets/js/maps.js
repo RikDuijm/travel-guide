3991bf3c5e320ac74f33363b980880442639b09d
Ubuntu <ubuntu@ip-172-31-86-251.ec2.internal>
2019-06-09 14:36:25 +0000

    Initial commit

---
+++ assets/js/maps.js
@@ -0,0 +1,148 @@
+
+var markersArequipa = [];
+
+
+function initMap(){
+
+
+
+// styling maps - generated with https://mapstyle.withgoogle.com/
+    var generalStyles = [
+        {
+            "featureType": "poi",
+            "elementType": "labels.text",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.attraction",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.business",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.government",
+            "stylers": [
+                {
+                    "visibility": "simplified"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.place_of_worship",
+            "stylers": [
+                {
+                    "visibility": "simplified"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.school",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        }
+    ]
+
+//options per map
+    var optionsLima = {
+        zoom: 12,
+        center: {lat:-12.122026,lng:-77.030747},
+        styles: generalStyles
+    };
+    var optionsArequipa = {
+        zoom: 12,
+        center: {lat:-16.398803,lng:-71.536916},
+        styles: generalStyles
+    };
+
+
+//place the map on HTML-page
+    var mapLima = new google.maps.Map(document.getElementById("lima-map"), optionsLima);
+    var mapArequipa = new google.maps.Map(document.getElementById("arequipa-map"), optionsArequipa);
+
+//create icons
+    var camera = "https://img.icons8.com/color/24/000000/old-time-camera.png";
+    var hotel = "https://img.icons8.com/color/24/000000/hotel-information.png";
+    var restaurant = "https://img.icons8.com/color/24/000000/dining-room.png";
+    var bar = "https://img.icons8.com/color/24/000000/bar.png";
+    var information = "https://img.icons8.com/color/24/000000/information.png";
+
+    var locationsArequipa = [
+        ['Monasterio', -16.395141, -71.536774, camera],['Juanita', -16.399810, -71.537825, camera], ['Casona Solar', -16.401777, -71.537058, hotel]
+    ];
+
+
+    var marker, i;
+    var infowindow = new google.maps.InfoWindow();
+
+    google.maps.event.addListener(mapArequipa, 'click', function() {
+        infowindow.close();
+    });
+
+
+    for (i = 0; i < locationsArequipa.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locationsArequipa[i][1], locationsArequipa[i][2]),
+            map: mapArequipa,
+            icon: locationsArequipa[i][3]
+        });
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent(locationsArequipa[i][0]);
+                infowindow.open(mapArequipa, marker);
+            }
+        })(marker, i));
+
+        markersArequipa.push(marker);
+    }
+
+    var markerCluster = new MarkerClusterer(mapArequipa, markersArequipa,
+        {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+
+
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
+
+function clickArequipa(id){
+    google.maps.event.trigger(markersArequipa[id], 'click');
+}
+
+
+
+
+
---
+++ assets/js/script.js
@@ -0,0 +1,114 @@
+$(document).ready(function() {
+    $(".opnieuw").click(function() {
+        $("#lima-intro").hide();
+        $("#arequipa-intro").hide();
+        $("#puno-intro").hide();
+        $("#cusco-intro").hide();
+    })
+
+
+/* ----------------------------------------------------------- Navigate Lima */
+
+
+    $("#lima-nav").click(function() {
+        $("#lima-intro").show();
+        $("#arequipa-intro").hide();
+        $("#puno-intro").hide();
+        $("#cusco-intro").hide();
+
+    });
+
+    $("#lima-attracties-header").click(function() {
+        $("#lima-attracties-text").toggle();
+    });
+
+
+    /* ----------------------------------------------------------- Navigate Arequipa */
+
+    $("#arequipa-nav").click(function() {
+        $("#arequipa-intro").show();
+        $("#lima-intro").hide();
+        $("#puno-intro").hide();
+        $("#cusco-intro").hide();
+    });
+
+    $(".arequipa-attracties-header").click(function() {
+        $(".arequipa-attracties").toggle();
+        $(".arequipa-hotels").hide();
+        $(".arequipa-restaurants").hide();
+        $(".arequipa-info").hide()
+    });
+
+    $(".arequipa-hotels-header").click(function() {
+        $(".arequipa-attracties").hide();
+        $(".arequipa-hotels").toggle();
+        $(".arequipa-restaurants").hide();
+        $(".arequipa-info").hide();
+    });
+    $(".arequipa-restaurants-header").click(function() {
+        $(".arequipa-attracties").hide();
+        $(".arequipa-hotels").hide();
+        $(".arequipa-restaurants").toggle();
+        $(".arequipa-info").hide();
+    });
+    $(".arequipa-info-header").click(function() {
+        $(".arequipa-attracties").hide();
+        $(".arequipa-hotels").hide();
+        $(".arequipa-restaurants").hide();
+        $(".arequipa-info").toggle();
+    });
+
+    $(function(){
+        $(".markers").on("click",function(){
+            $(this).next().toggle();
+        });
+    });
+/*
+
+ $(".destination-headers").click(function() {
+        $(this).children("ol").toggle();
+    });
+
+    $(".markers").click(function() {
+        $(this).children("li").toggle();
+    });
+
+    $("#arequipa-attracties-header").click(function() {
+        $("#arequipa-attracties-text").toggle();
+    });
+
+    $("#arequipa-attracties-header").click(function() {
+        $("#arequipa-attracties-text").toggle();
+    });
+
+
+
+    $("#arequipa-hotels-header").click(function() {
+        $("#arequipa-hotels-text").toggle();
+    });
+
+    $("#arequipa-restaurants-header").click(function() {
+        $("#arequipa-restaurants-text").toggle();
+    });
+
+    $("#arequipa-handig-header").click(function() {
+        $("#arequipa-handig-text").toggle();
+    });
+
+    $("#arequipa-juanita-icon").click(function() {
+        $("#arequipa-attracties-text").toggle();
+    });
+*/
+    /* ----------------------------------------------------------- Map Arequipa */
+
+    /* ----------------------------------------------------------- Scroll down */
+
+    /* code used from https://www.daddydesign.com/wordpress/how-to-smoothly-scroll-to-a-specific-element-using-jquery/ */
+    $('.scroll_to').click(function(e) {
+        var jump = $(this).attr('href');
+        var new_position = $(jump).offset();
+        $('html, body').stop().animate({ scrollTop: new_position.top }, 500);
+        e.preventDefault();
+    });
+
+});
